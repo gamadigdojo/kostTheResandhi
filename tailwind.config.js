7cd1e116b03cfeca9a16640dbfc4dc34e955160d
gamadigdojo <gamadigdojo@gmail.com>
2024-08-06 16:04:11 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        main: '#04151f',
+        third: '#183a37',
+        second: '#0e5178',
+      },
+    },
+  },
+  plugins: [],
+}
